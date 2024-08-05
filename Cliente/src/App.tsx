428cf5152f40f9439f5a6b624057411ada956db4
jhonbg09 <jhon.betancourthg09@gmail.com>
2024-08-05 15:08:50 -0500

    agragndo header

--- Cliente/src/App.tsx
+++ Cliente/src/App.tsx
@@ -1,13 +1,13 @@
 
 import './App.css'
+import Header from './components/Header'
 
 function App() {
-  
-
   return (
-    <>
-     
-    </>
+    <div className='m-0 w-screen h-screen'>
+      <Header/>
+      <p>Hola desde el app</p>
+    </div>
   )
 }
 
---
+++ Cliente/src/components/Header.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Header() {
+  return (
+    <div className="bg-sky-500 m-0 p-0 w-screen h-10">
+      <h1 className='text-2xl uppercase text-center m-0 h-0 center'>Administador de productos</h1>
+    </div>
+  )
+}
