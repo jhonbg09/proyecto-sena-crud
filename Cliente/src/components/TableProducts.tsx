4b3fae3d193540ddc22fb75a7f8241bcfabb0c62
jhonbg09 <jhon.betancourthg09@gmail.com>
2024-08-06 11:59:33 -0500

    Agregando hook

--- Cliente/src/components/TableProducts.tsx
+++ Cliente/src/components/TableProducts.tsx
@@ -15,8 +15,13 @@ export default function TableProducts() {
         <tbody>
           {data.map((products: Product) => (
             <tr key={products.id} className="flex">
-              <td className="p-3 mx-10">{products.product}</td>
-              <td className="p-3 mx-10">{products.price}</td>
+              <td className="p-3 mx-10">
+                <p className="text-center">{products.product}</p>
+              </td>
+              <td className="p-3 mx-10">
+                <p>${products.price}</p>
+              </td>
+              <td className="p-3 mx-10">{products.available}</td>
               <td className="p-3 mx-10">{products.available}</td>
             </tr>
           ))}
